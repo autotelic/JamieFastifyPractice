b0f69fc977b741016e1f4a48c52a9256538bc4a7
Jamie Li <jamie@autotelic.com>
2022-06-14 12:35:40 -0700

    feat: create get users route
    refactor: fix users.js import
    
    style: fix tab sizing, missing semi colons

---
+++ ex02/index.js
@@ -0,0 +1,14 @@
+import Fastify from 'fastify';
+import usersRoute from './routes/users.js'
+
+function startServer() {
+	const fastify = Fastify({
+			logger: true
+	})
+
+  fastify.register(usersRoute)
+
+  return fastify
+}
+
+export default startServer;
