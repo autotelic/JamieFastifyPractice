ac21ee7ae63d2d4a65212d66d6dfaafc74d7d66a
Jamie Li <jamie@autotelic.com>
2022-06-14 11:08:40 -0700

    refactor: changed to use async/await

--- 01.js
+++ 01.js
@@ -9,16 +9,32 @@
 
 import Fastify from 'fastify';
 const fastify = Fastify({
-    logger:true
+	logger:true
 });
 
-fastify.get('/', function (req, res) {
-    res.send({ hello: 'world' })
-});
+// fastify.get('/', function (req, res) {
+//   res.send({ hello: 'world' })
+// });
 
-fastify.listen({ port:3000 }, function (err, address) {
-    if(err) {
-        fastify.log.error(err)
-        process.exit(1);
-    }
-});
+// fastify.listen({ port:3000 }, function (err, address) {
+//   if(err) {
+//     fastify.log.error(err)
+//     process.exit(1);
+//   }
+// });
+
+// async variant
+fastify.get('/', async (req,res) => {
+  return { hello:'world' }
+})
+
+const start = async () => {
+	try {
+		await fastify.listen(3000)
+	} catch (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
+}
+
+start()
