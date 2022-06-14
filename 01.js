711afe389f9346badd719ac46b0dd7890aa34ceb
Jamie Li <jamie@autotelic.com>
2022-06-14 10:52:34 -0700

    style: added semi colons

--- 01.js
+++ 01.js
@@ -13,12 +13,12 @@ const fastify = Fastify({
 });
 
 fastify.get('/', function (req, res) {
-    reply.send({hello: 'world'})
-})
+    reply.send({hello: 'world'});
+});
 
 fastify.listen({port:3000}, function (err, address) {
     if(err) {
-        fastify.log.error(err)
-        process.exit(1)
+        fastify.log.error(err);
+        process.exit(1);
     }
-})
+});
