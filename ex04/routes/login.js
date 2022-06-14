05bc407b6d79991c584d22f0dc0a43df887bda87
Jamie Li <jamie@autotelic.com>
2022-06-14 15:54:11 -0700

    create ex04, fix spelling in 03 users.js, delete remnant 01.js

---
+++ ex04/routes/login.js
@@ -0,0 +1,13 @@
+import S from 'fluent-json-schema';
+
+const schema = S.object()
+  .prop('username', S.string().required())
+  .prop('password', S.string().required())
+
+async function login(fastify) {
+  fastify.post('/login', async (req) => {
+    req.log.info('Login post called')
+    const { username, password } = req.body
+    return { username, password }
+  })
+}
--- ex04/routes/users.js
+++ ex04/routes/users.js
@@ -1,7 +1,7 @@
 async function users(fastify) {
   fastify.get('/users', async (req) => {
     req.log.info('Users route called')
-
+    
     return [{username: "Jamie"}, {username: "Ivan"}, {username: "Joseph"}]
   })
 }
