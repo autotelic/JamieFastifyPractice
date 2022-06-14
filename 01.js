a488c8b864946615d9572ed7b330978b9d09012e
Jamie Li <jamie@autotelic.com>
2022-06-14 10:51:11 -0700

    feat: create get / route

--- 01.js
+++ 01.js
@@ -1,5 +1,24 @@
+// tasks
+// Exposes a GET / route
+// Listens on port 3000
+// Responds with the JSON object
+// {
+//   "hello": "world"
+// }
+// 🏆 use ES modules!
+
 import Fastify from 'fastify';
 const fastify = Fastify({
     logger:true
 });
 
+fastify.get('/', function (req, res) {
+    reply.send({hello: 'world'})
+})
+
+fastify.listen({port:3000}, function (err, address) {
+    if(err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+})
