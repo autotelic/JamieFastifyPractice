66bdf1799fd84e9b1ea7177fc69dd3044ce95eae
Jamie Li <jamie@autotelic.com>
2022-06-15 10:48:49 -0700

    refactor login test, remove redundant test, fix naming errors

--- ex05/test/login.test.js
+++ ex05/test/login.test.js
@@ -1,45 +1,31 @@
 'use strict'
 import t from 'tap';
-
 import startServer from '../index.js';
 
 const { test } = t;
 
 test('POST /login', async t => {
+  t.test('returns 400 with invalid payload', async t => {
+    const fastify = startServer()
 
-  t.test('returns 400 with invalid username in payload', async t => {
-    const fastify = startServer();
     const res = await fastify.inject({
-      method: 'POST',
       url: '/login',
+      method: 'POST',
       body: {
         name: 'Jamie',
-        password: 'Jamie'
+        passcode: 'Jamie'
       }
     })
 
     t.equal(res.statusCode, 400, 'returns status code 400')
   })
 
-  t.test('returns 400 with invalid password in payload', async t => {
-    const fastify = startServer();
+  t.test('returns data with valid payload',  async t => {
+    const fastify = startServer()
+
     const res = await fastify.inject({
-      method: 'POST',
       url: '/login',
-      body: {
-        username: 'Jamie',
-        pass: 'Jamie'
-      }
-    })
-
-    t.equal(res.statusCode, 400, 'returns status code 400')
-  })
-
-  t.test.apply('returns data with valid payload',  async t => {
-    const fastify = startServer();
-    const res = await fastify.inject({
       method: 'POST',
-      urls: '/login',
       body: {
         username: 'Jamie',
         password: 'Jamie'
