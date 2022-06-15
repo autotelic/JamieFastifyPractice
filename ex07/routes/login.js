4d90ca9f6d9ed09e7b62c29b94b1701d1522055c
Jamie Li <jamie@autotelic.com>
2022-06-15 12:42:28 -0700

    add authentication to login.js

--- ex07/routes/login.js
+++ ex07/routes/login.js
@@ -1,16 +1,26 @@
 import S from 'fluent-json-schema';
+import errors from 'http-errors'
 
 const schema = {
   body: S.object()
     .prop('username', S.string().required())
-    .prop('password', S.string().required())
+    .prop('password', S.string().required()),
+  response: {
+    200: S.object()
+      .prop('token', S.string().required())
+  },
 }
 
 async function login(fastify) {
-  fastify.post('/login', async (req) => {
+  fastify.post('/login', { schema },async (req) => {
     req.log.info('Login post called')
     const { username, password } = req.body
-    return { username, password }
+
+    if (username === password) {
+      return {token: fastify.jwt.sign({ username })}
+    }
+    
+    throw errors.Unauthorized()
   })
 }
 
