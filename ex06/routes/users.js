cb448252c295bdcba08df86ae1df21a0ad37a8f6
Jamie Li <jamie@autotelic.com>
2022-06-15 11:27:17 -0700

    add response validation

--- ex06/routes/users.js
+++ ex06/routes/users.js
@@ -1,8 +1,21 @@
+import S from 'fluent-json-schema'
+
+const schema = {
+  response: {
+    200: S.array.items(
+      S.object().prop('username', S.string().required())
+    )
+  }
+}
+
 async function users(fastify) {
-  fastify.get('/users', async (req) => {
+  fastify.get('/users', { schema }, async (req) => {
     req.log.info('Users route called')
     
-    return [{username: "Jamie"}, {username: "Ivan"}, {username: "Joseph"}]
+    // valid response
+    return [{username: 'Jamie'}, {username: 'Ivan'}, {username: 'Joseph'}]
+    // invalid response
+    // return [{name: 'Jamie'}, {name: 'Ivan'}, {name: 'Joseph'}]
   })
 }
 
